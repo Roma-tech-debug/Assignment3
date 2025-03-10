044a0ca24d104b35a0f98292cd80d801c5674c29
Roma-tech-debug <romanpreetkaur050198@gmail.com>
2025-03-09 19:52:58 -0600

    Index file and update dependencies

---
+++ components/HomeScreen.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, StyleSheet, ActivityIndicator, TouchableOpacity } from 'react-native';
+import axios from 'axios';
+import RNPickerSelect from 'react-native-picker-select';
+import { useNavigation } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons'; // Importing icons
+
+
+
+export default HomeScreen;
+
+
